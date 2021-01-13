fff5a490a097663eb88a352295cf0307dbc694e8
crito <crito@fnordpipe.org>
2021-01-13 06:16:58 +0100

    cleanup old code and starting from scratch

--- src/include/luksd_cryptsetup.h
+++
@@ -1,8 +0,0 @@
-#ifndef LUKSD_CRYPTSETUP_H
-#define LUKSD_CRYPTSETUP_H
-
-#include "luksd.h"
-
-int luksd_cryptsetup_read_device(luksd_device_t *device);
-
-#endif
--- src/include/rluksd_luksd.h
+++
@@ -1,6 +0,0 @@
-#ifndef RLUKSD_LUKSD_H
-#define RLUKSD_LUKSD_H
-
-int rluksd_luksd_connect(char *socket_file);
-
-#endif
